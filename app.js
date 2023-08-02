a53404ed7a1ad63b1cbd69bb21123ca93c437176
GitEsense <klg-hunter@mail.ru>
2023-08-02 13:08:51 +0200

    изменены файлы под шаблон урока

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-const divElement = document.querySelector(".test");
-
-function logText(text) {
-  const element = document.createElement("p");
-  element.innerHTML = text;
-  divElement.append(document.createElement("br"));
-  divElement.insertAdjacentElement("beforeend", element);
-}
-
-logText(`<img src="./logo.svg" alt="" />`);
+console.log("Hello World!");
