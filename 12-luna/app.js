dbe3d1f71a7eccb35ba20a8ccbda2f4cf1541879
GitEsense <klg-hunter@mail.ru>
2023-08-09 10:05:12 +0200

    двенадцатая ступень - домашнее задание (исправления)

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -3,49 +3,34 @@
     написать функцию проверки номера карты алгоритмом Луна. В функцию передаётся карта:
     4561-2612-1234-5464, а функция возвращает true, если карта проходит алгоритм и false, если нет
 */
-function cardLunaValidation(card) {
+function cardLunaValidate(card) {
     const cardNumber = card
         .replaceAll('-', '')
         .split('')
         .map((x) => Number(x));
+    if (cardNumber.includes(NaN)) {
+        return NaN;
+    }
     const isEven = (cardNumber.length - 1) % 2 === 0;
 
-    for (let i = 0; i < cardNumber.length; i++) {
-        if (isNaN(cardNumber[i])) {
-            return false;
-        }
-        if (i % 2 == isEven) {
-            cardNumber[i] =
-                cardNumber[i] * 2 > 9
-                    ? cardNumber[i] * 2 - 9
-                    : cardNumber[i] * 2;
-        }
+    for (let i = Number(isEven); i < cardNumber.length; i = i + 2) {
+        cardNumber[i] =
+            cardNumber[i] * 2 > 9 ? cardNumber[i] * 2 - 9 : cardNumber[i] * 2;
     }
     const sum = cardNumber.reduce((total, el) => total + el);
     return sum % 10 === 0;
 }
+const card = '234s834503458353';
 const card1 = '2342834503458353';
 const card2 = '4561-2612-1234-5464';
 const card3 = '4561-2612-1534-5464';
 
-console.log(
-    `Карта с номером ${card1}: ${
-        cardLunaValidation(card1)
-            ? 'прошла алгоритм'
-            : 'не смогла пройти алгоритм'
-    }`
-);
-console.log(
-    `Карта с номером ${card2}: ${
-        cardLunaValidation(card2)
-            ? 'прошла алгоритм'
-            : 'не смогла пройти алгоритм'
-    }`
-);
-console.log(
-    `Карта с номером ${card3}: ${
-        cardLunaValidation(card3)
-            ? 'прошла алгоритм'
-            : 'не смогла пройти алгоритм'
-    }`
-);
+function resultTemplate(card) {
+    const startString = `Карта с номером: ${card}`;
+    const endString = `получила результат: ${cardLunaValidate(card)}`;
+    return `${startString} ${endString}`;
+}
+console.log(resultTemplate(card));
+console.log(resultTemplate(card1));
+console.log(resultTemplate(card2));
+console.log(resultTemplate(card3));
