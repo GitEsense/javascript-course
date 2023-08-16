8a10ba7cad475afda0e3cbd9b4c1f3d829be427e
GitEsense <klg-hunter@mail.ru>
2023-08-16 11:45:59 +0200

    шестнадцатая ступень - домашнее задание (исправление 2)

--- 16-another-tasks/app.js
+++ 16-another-tasks/app.js
@@ -1,11 +1,16 @@
 //  Домашнее задание
 /*
-    Написать объект ToDoList, который хранит в себе задачи 
-        {'name': 'Помыть посуду', id: 1, order: 1} и имеет методы:
-        -   Добавить задачу
-        -   Удалить задачу по id
-        -   Обновить имя или приоритет по id
-        -   Отсортировать задачи по приоритету
+    Возьмите объект из предыдущего домашнего задания и последовательно
+    примените все методы его на новый объект:
+    
+    const newTask {
+        tasks:[{
+            id: 1,
+            name: 'тест',
+            description: 'описание',
+            order: 0
+        }]
+    }
 */
 let countAdd = 0; //  Количество пройденных итераций добавления
 let countUpdate = 0; //  Количество пройденных итераций обновления
@@ -17,8 +22,10 @@ function addActionToObject(obj, functions) {
         obj[key] = functions[key].bind(obj);
     }
 }
-//  Методы объекта
-const objectFunctions = {
+
+const toDoList = {
+    tasks: [],
+    //  Методы объекта
     //  Добавление задачи, где data = объект данных
     addTask(data) {
         countAdd++;
@@ -26,7 +33,7 @@ const objectFunctions = {
         if (!data) {
             return message;
         }
-        const lastId = [...this.sortTasksById()].at(-1)?.id ?? 0;
+        const lastId = this.tasks.sort((a, b) => a.id - b.id).at(-1)?.id ?? 0;
         data.id = lastId + 1;
         this.tasks.push({ ...data });
         message = `${countAdd}. Задача с id: ${data.id} успешно добавлена`;
@@ -42,11 +49,11 @@ const objectFunctions = {
         }
         const isExitsts = this.tasks.some((task) => task.id == id);
         if (!isExitsts) {
-            message = `${countRemove}. Задача c ${id} не найдена`;
+            message = `${countRemove}. Задача с id: ${id} - не найдена`;
             return message;
         }
         this.tasks = this.tasks.filter((task) => task.id !== id);
-        message = `${countRemove}. Задача с ${id} успешно удалена`;
+        message = `${countRemove}. Задача с id: ${id} - успешно удалена`;
         return message;
     },
     //  Обновление задачи по ID, где data = объект новых данных
@@ -57,7 +64,7 @@ const objectFunctions = {
             return message;
         }
         const index = this.tasks.findIndex((task) => task.id == id);
-        message = `Задача c id: ${id}`;
+        message = `Задача с id: ${id} -`;
         if (index === -1) {
             message = `${countUpdate}. ${message} не найдена`;
             return message;
@@ -84,137 +91,49 @@ const objectFunctions = {
     },
 };
 
-const toDoList = {
-    tasks: [],
-};
-addActionToObject(toDoList, objectFunctions); //  Добавляем методы объекту toDoList
-
 const newTask = {
     tasks: [],
 };
-addActionToObject(newTask, objectFunctions); //  Добавляем методы объекту newTask
 
+newTask.addTask = toDoList.addTask;
+newTask.removeTaskById = toDoList.removeTaskById;
+newTask.updateTaskById = toDoList.updateTaskById;
+newTask.sortTasksById = toDoList.sortTasksById;
+
+const newTaskAddTask = toDoList.addTask.bind(newTask);
+const newTaskRemoveTaskById = toDoList.removeTaskById.bind(newTask);
+const newTaskUpdateTaskById = toDoList.updateTaskById.bind(newTask);
+const newTaskSortTasksById = toDoList.sortTasksById.bind(newTask);
+
+let data = {
+    name: 'Новая для теста',
+    description: 'Тестовое описание',
+    order: 10,
+};
+
+console.log(newTask.addTask(data));
+console.log(newTaskAddTask(data));
+console.log(toDoList.addTask.apply(newTask, [data]));
+console.log(toDoList.addTask.call(newTask, data));
+
+console.log(newTask.removeTaskById(getRandom(1, 10)));
+console.log(newTaskRemoveTaskById(getRandom(1, 10)));
+console.log(toDoList.removeTaskById.apply(newTask, [getRandom(1, 10)]));
+console.log(toDoList.removeTaskById.call(newTask, getRandom(1, 10)));
+
+data = {
+    name: 'Обновление для теста',
+    description: 'Тестовое описание обновленное',
+    order: 10,
+};
+
+console.log(newTask.updateTaskById(getRandom(1, 10), data));
+console.log(newTaskUpdateTaskById(getRandom(1, 10), data));
+console.log(toDoList.updateTaskById.apply(newTask, [getRandom(1, 10), data]));
+console.log(toDoList.updateTaskById.call(newTask, getRandom(1, 10), data));
+
+console.log(newTask);
 //  Функция генерации случайных чисел
 function getRandom(start, end = 10) {
     return Math.round(Math.random() * (end - start) + start);
 }
-
-//  Функция создания шаблона сообщения - разделителя
-function templateMessage(text, count) {
-    const countLine = 50;
-    const leftRigthLine = (countLine - text.length) / 2;
-    const minusLine = `${count ?? ''}-РАЗ(-А)`.length + 2;
-    console.log('-'.repeat(countLine));
-    console.log(
-        `${'-'.repeat(leftRigthLine)}${text}--${
-            count
-                ? `${count}-РАЗ(-А)` + '-'.repeat(leftRigthLine - minusLine)
-                : '-'.repeat(leftRigthLine)
-        }`
-    );
-    console.log('-'.repeat(countLine));
-}
-
-//  Генерация тестовых данных
-const randomAddCount = getRandom(5, 10); //  Количество добавлений тасков
-const randomUpdateCount = getRandom(2, 4); //  Количество обновлений тасков
-const randomRemoveCount = getRandom(2, 4); //  Количество удалений тасков
-//  Диапазон генерации числа из рандома, где rndId[0] - start, rndId[1] - end
-const rndId = [1, 10];
-//  Диапазон генерации числа из рандома, где rndname[0] - start, rndname[1] - end
-const rndname = [1, 10];
-//  Диапазон генерации числа из рандома, где rndorder[0] - start, rndorder[1] - end
-const rndorder = [1, 10];
-
-//  Вызов функции создания сообщения - разделителя
-templateMessage('ДОБАВЛЕНИЕ', randomAddCount);
-
-//  Цикл генерации данных для добавления тасков, где randomAddCount - количество итераций
-for (let i = 0; i < randomAddCount; i++) {
-    const name = 'Пробный текст #' + getRandom(...rndname);
-    const description = 'Пробное описание #' + getRandom(...rndname);
-    const order = getRandom(...rndorder);
-    const body = {};
-    if (i % 3 === 0) {
-        body.title = name;
-        body.priority = order;
-    } else {
-        body.name = name;
-        body.description = description;
-        body.order = order;
-    }
-
-    const res = i % 3 === 0 ? toDoList.addTask(body) : newTask.addTask(body);
-    console.log(i % 3 === 0 ? 'newTask' : 'toDoList', res);
-}
-
-//  Вызов функции создания сообщения - разделителя
-templateMessage('ОБНОВЛЕНИЕ', randomUpdateCount);
-
-//  Цикл генерации данных для обновления тасков, где randomUpdateCount - количество итераций
-for (let i = 0; i < randomUpdateCount; i++) {
-    const id = getRandom(...rndId);
-    const name = getRandom(...rndname);
-    const order = getRandom(...rndorder);
-    const description = getRandom(...[1, 25]);
-    const bodyName =
-        name % getRandom(1, 5) === 0 ? 'Обновлённый текст #' + name : null;
-    const bodyDescription =
-        description % getRandom(1, 5) === 0
-            ? 'Обновлённое описание #' + description
-            : null;
-    const bodyOrder = order % getRandom(1, 5) === 1 ? order : null;
-    const body = {};
-    if (i % 3 === 0) {
-        body.title = bodyName;
-        body.priority = bodyOrder;
-    } else {
-        body.name = bodyName;
-        body.description = bodyDescription;
-        body.order = bodyOrder;
-    }
-    if (order >= 4 && order <= 6) {
-        body.step = getRandom(1, 10);
-    } else if (order >= 7 && order <= 8) {
-        body.random = getRandom(1, 500);
-    } else if (order === 9) {
-        body.loworder = true;
-    }
-    const res =
-        i % 3 === 0
-            ? toDoList.updateTaskById(id, body)
-            : newTask.updateTaskById(id, body);
-    console.log(i % 3 === 0 ? 'newTask' : 'toDoList', res);
-}
-
-//  Вызов функции создания сообщения - разделителя
-templateMessage('УДАЛЕНИЕ', randomRemoveCount);
-
-//  Цикл генерации данных для удаления тасков, где randomRemoveCount - количество итераций
-for (let i = 0; i < randomRemoveCount; i++) {
-    const id = getRandom(...rndId);
-    const res =
-        i % 3 === 0 ? newTask.removeTaskById(id) : toDoList.removeTaskById(id);
-    console.log(i % 3 === 0 ? 'newTask' : 'toDoList', res);
-}
-//  Вызов функции создания сообщения - разделителя
-templateMessage('END');
-console.log({ countAdd, countRemove, countUpdate });
-console.log('-----------------ORIGINAL - TODO-------------------');
-console.log([...toDoList.tasks]);
-console.log('-----------------ORIGINAL - NEWTASK-------------------');
-console.log([...newTask.tasks]);
-
-const isDescending = getRandom(0, 1); //  Сортировать по возрастанию или убыванию
-console.log(
-    `-----------------SORTED BY ID - TODO ${
-        isDescending ? '(DESC)' : ''
-    }-------------------`
-);
-console.log([...toDoList.sortTasksById(isDescending)]);
-console.log(
-    `-----------------SORTED BY ID - NEWTASK ${
-        isDescending ? '(DESC)' : ''
-    }-------------------`
-);
-console.log([...newTask.sortTasksById(isDescending)]);
