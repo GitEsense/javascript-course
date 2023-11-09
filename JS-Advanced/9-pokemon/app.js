e5fb98bfe09a0a08e36a129ea905415966068f35
GitEsense <klg-hunter@mail.ru>
2023-11-09 16:23:36 +0200

    Асинхронный JavaScript - домашнее задание (Step - 11) - v.2

--- JS-Advanced/9-pokemon/app.js
+++ JS-Advanced/9-pokemon/app.js
@@ -7,6 +7,13 @@
 
         После получения, получите информацию о первой его ability по ссылке, которая приходит при первом запросе. Там найдите описание на английском и выведите в консоль.
 */
+
+/* 
+    ------------------------------------------
+    VARIANT 1
+    ------------------------------------------
+*/
+
 function jsonParse(response) {
     try {
         return JSON.parse(response);
@@ -41,3 +48,38 @@ function getEffect(url) {
     });
 }
 getPokemon();
+
+/* 
+    ------------------------------------------
+    VARIANT 2
+    ------------------------------------------
+*/
+
+function request(url, callback, method = 'GET', body = {}) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+    xhr.send(body);
+
+    xhr.addEventListener('load', function () {
+        const json = jsonParse(this.responseText);
+        callback(json, printEffect);
+    });
+}
+
+function parseEffect(json, callback) {
+    const { abilities } = json;
+    const { ability } = abilities[0];
+    const { url } = ability;
+
+    request(url, callback);
+}
+
+function printEffect(json) {
+    const { effect_entries } = json;
+    const { effect } = effect_entries.find(({ language }) => language.name === 'en');
+    console.log(effect);
+}
+
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+request(url, parseEffect);
