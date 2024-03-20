5a5797f24749ba8688b682c6a457f77cb772e0b4
GitEsense <klg-hunter@mail.ru>
2024-03-20 13:24:07 +0200

    Небольшие улучшательства украшательства

--- JS-Advanced/14-user-task/scripts/modules/task.js
+++ JS-Advanced/14-user-task/scripts/modules/task.js
@@ -10,6 +10,12 @@ export class Task {
             setTimeout(() => {
                 this.#wrapper.innerHTML += char;
             }, i * 100);
-        });
+            setTimeout(() => {
+                let lastIndex = this.#wrapper.innerHTML.length - 1;
+                if (lastIndex >= 0) {
+                    this.#wrapper.innerHTML = this.#wrapper.innerHTML.substring(0, lastIndex);
+                }
+            }, (this.message.length + i) * 100);
+        }, this.message.length * 2 * 100 + 200);
     }
 }
--- JS-Basic/11-date-array/app.js
+++ JS-Basic/11-date-array/app.js
@@ -6,8 +6,16 @@
     -   ['10-02-2022','12-11-2023']
 */
 
+/*
+    VARIANT 1
+*/
+
 const stringArray = [
-    '10-02-2022',
+    '31-01-2022',
+    '29-02-2024',
+    '29-02',
+    '2024-12-03',
+    '29-02-2023',
     'тест',
     '11/12/2023',
     '00/13/2022',
@@ -18,48 +26,59 @@ const stringArray = [
     '012/011/20ts',
 ];
 
-function canDelete(array, fn) {
-    return array
-        .map((element) => {
-            let splitArray = element.split('-');
-            if (splitArray.length < 3) {
-                splitArray = element.split('/');
-                if (splitArray.length === 3) {
-                    const [day] = splitArray.splice(1, 1);
-                    splitArray.unshift(day);
-                }
-            }
-            return splitArray;
-        })
-        .filter((dateArray) => fn(dateArray))
+function getDates(array, fn) {
+    const resultArr = [...array];
+    return resultArr
+        .map(stringToArray)
+        .filter(fn)
         .map((array) => array.join('-'));
 }
 
-function filterPattern(array) {
-    const [day, month, year] = array;
-    const leapYear = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
-    const daysPerMonth = [
-        31,
-        leapYear ? 29 : 28,
-        31,
-        30,
-        31,
-        30,
-        31,
-        31,
-        30,
-        31,
-        30,
-        31,
-    ];
+function stringToArray(dateString) {
+    let [day, month, year] = dateString.split('-');
+    if (!year) {
+        [month, day, year] = dateString.split('/');
+    }
+    if (!year || isNaN(day) || isNaN(month) || isNaN(year)) {
+        return null;
+    }
 
-    return (
-        day > 0 &&
-        day <= daysPerMonth[month - 1] &&
-        month > 0 &&
-        month < 13 &&
-        year > 1900
-    );
+    return [day, month, year];
 }
 
-console.log(canDelete(stringArray, filterPattern));
+function checkCorrectDate(array) {
+    if (array === null) {
+        return false;
+    }
+
+    const LONG_MONTH_ARRAY = [1, 3, 5, 7, 8, 10, 12];
+
+    const [day, month, year] = array.map(Number);
+
+    const isLeapYear = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+
+    // check month
+    if (month > 12 || month < 1) {
+        return false;
+    }
+
+    // check day
+    if (day > 31 || day < 1) {
+        return false;
+    }
+
+    if (day === 31 && !LONG_MONTH_ARRAY.includes(month)) {
+        return false;
+    }
+
+    // check February for correct day and leap year
+    if (month === 2) {
+        if (day === 30 || (day === 29 && !isLeapYear)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(getDates(stringArray, checkCorrectDate));
