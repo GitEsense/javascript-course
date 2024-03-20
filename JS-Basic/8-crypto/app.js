85e90618135c056a53afdf2e53688a47edc39d3f
GitEsense <klg-hunter@mail.ru>
2024-03-20 15:44:37 +0200

    Ещё немного фишек плюшек улучшалок

--- JS-Basic/8-crypto/app.js
+++ JS-Basic/8-crypto/app.js
@@ -1,21 +1,58 @@
-//  Домашнее задание - Массивы
-function transformPassword(password) {
-    const strArray = password.split('');
-    const firstHalfOfPassword = strArray.slice(0, strArray.length / 2);
-    const secondHalfOfPassword = strArray.slice(strArray.length / 2);
-
-    firstHalfOfPassword.reverse();
-    secondHalfOfPassword.reverse();
-    for (let i = 1; i < secondHalfOfPassword.length; i++) {
-        secondHalfOfPassword.push(secondHalfOfPassword[i]);
-        secondHalfOfPassword.splice(i, 1);
+/**
+ * function encrypt / decrypt string password
+ * @param {string} password - A string password
+ */
+function crypt(password) {
+    if (!password || !password.length) {
+        return NaN;
     }
-    return firstHalfOfPassword.concat(secondHalfOfPassword).join('');
-}
-function checkPassword(str1, password) {
-    return password === transformPassword(str1);
+    const splitedArray = password.split('');
+    if (splitedArray.length < 8) {
+        return false;
+    }
+
+    const middleIndex = Math.floor(splitedArray.length / 2);
+    const firstHalf = splitedArray.slice(0, middleIndex).reverse();
+    const secondHalf = splitedArray.slice(middleIndex).reverse();
+    const length = secondHalf.length;
+    [secondHalf[1], secondHalf[length - 2]] = [secondHalf[length - 2], secondHalf[1]];
+
+    const encryptedPassword = firstHalf.concat(secondHalf);
+    return encryptedPassword.join('');
 }
 
-console.log(transformPassword('password')); //  ssapdorw
-console.log(checkPassword('ssapdorw', 'password')); //  true
-console.log(checkPassword('ssapdorw', 'not')); // false
+function checkPassword(encryptedPassword, originalPassword) {
+    if (!encryptedPassword || !originalPassword) {
+        return false;
+    }
+    return originalPassword === crypt(encryptedPassword);
+}
+
+function getRandomNumber(min = 0, max = 86) {
+    return Math.ceil(Math.random() * (max - min)) + min;
+}
+
+function getRandomPassword(length = 12) {
+    const symbolArray = Array.from({ length: 87 }, (_, index) => String.fromCharCode(index + 33));
+    console.log(symbolArray);
+    const password = Array.from({ length }, () => symbolArray[getRandomNumber()]);
+    return password.join('');
+}
+
+/*
+    logging and test function with console
+*/
+
+const password = getRandomPassword();
+const encrypted = crypt(password);
+
+console.log('Пароль:', password);
+console.log('Зашифрованный пароль:', encrypted);
+console.log('Проверка пароля:', checkPassword(encrypted, password));
+
+const password2 = getRandomPassword();
+const encrypted2 = crypt(password2).substring(1);
+
+console.log('Пароль:', password2);
+console.log('Зашифрованный пароль:', encrypted2);
+console.log('Проверка пароля:', checkPassword(encrypted2, getRandomPassword()));
--- JS-Basic/9-sort-loops/app.js
+++ JS-Basic/9-sort-loops/app.js
@@ -7,20 +7,34 @@
     -   Нужно использовать 2 цикла, вложенных друг в друга
     -   Нужно сравнивать и менять элементы
 */
-const arr = [1, 40, -5, 10, 0];
-function sortArrayInAscending(array) {
-    const resultArray = [...array];
-    for (let i = 0; i < resultArray.length; i++) {
-        for (let j = i; j < resultArray.length; j++) {
-            if (resultArray[i] > resultArray[j]) {
-                [resultArray[i], resultArray[j]] = [
-                    resultArray[j],
-                    resultArray[i],
-                ];
+const numbers = Array.from({ length: 1000 }, () => {
+    const max = 1000000;
+    const min = 1000;
+    const factor = Math.random() > 0.5 ? 1 : -1;
+    const randomNumber = Math.ceil(Math.random() * (max - min)) + min;
+    return randomNumber * factor;
+});
+
+function orderTemplate(firstNumber, secondNumber, desc) {
+    if (!desc) {
+        return firstNumber > secondNumber;
+    }
+    return firstNumber < secondNumber;
+}
+
+function sortArrayInAscending(incomingArray, desc = false) {
+    const array = [...incomingArray];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i; j < array.length; j++) {
+            const isExchange = orderTemplate(array[i], array[j], desc);
+            if (isExchange) {
+                [array[i], array[j]] = [array[j], array[i]];
             }
         }
     }
-    return resultArray;
+    return array;
 }
 
-console.log(sortArrayInAscending(arr));
+console.log(numbers);
+console.log('Отсортированный по возрастанию массив:', sortArrayInAscending(numbers));
+console.log('Отсортированный по убыванию массив:', sortArrayInAscending(numbers, true));
